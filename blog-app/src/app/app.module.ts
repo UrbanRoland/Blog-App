593d79df41abe1d536626d4281f3f179bacaa6fb
UrbanRoland <urolir@gmail.com>
2021-01-24 13:36:28 +0100

    feat: added search button

--- blog-app/src/app/app.module.ts
+++ blog-app/src/app/app.module.ts
@@ -13,6 +13,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, MainComponent, HeaderComponent],
@@ -26,6 +28,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatMenuModule,
     MatButtonModule,
     MatToolbarModule,
+    MatInputModule,
+    MatFormFieldModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- blog-app/src/app/components/header/header.component.ts
+++ blog-app/src/app/components/header/header.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter,
+  ViewChild,
+  ElementRef,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  searchIcon = 'search';
 
-  constructor() { }
+  @ViewChild('searchInput', { read: ElementRef })
+  private searchInput: ElementRef;
 
-  ngOnInit(): void {
+  interactedWithSearch = false;
+
+  @Output()
+  searchEvent = new EventEmitter<{
+    query?: string;
+    action: 'SEARCH' | 'CLEAR';
+  }>();
+
+  constructor() {}
+
+  toggleSearch() {
+    const searchContainer = document.getElementById('search-container');
+    this.toggleClass(searchContainer, 'open');
+    this.searchIcon = this.hasClass(searchContainer, 'open')
+      ? 'clear'
+      : 'search';
+    if (!this.hasClass(searchContainer, 'open') && this.interactedWithSearch) {
+      this.searchEvent.emit({ action: 'CLEAR' });
+      this.interactedWithSearch = false;
+      this.searchInput.nativeElement.value = '';
+    }
   }
 
+  private toggleClass(elem, className) {
+    this.hasClass(elem, className)
+      ? elem.classList.remove(className)
+      : elem.classList.add(className);
+  }
+
+  private hasClass(elem, className): boolean {
+    return elem.classList.contains(className);
+  }
+
+  search() {
+    const searchTerm = this.searchInput.nativeElement.value;
+    this.searchEvent.emit({ query: searchTerm, action: 'SEARCH' });
+    this.interactedWithSearch = true;
+  }
 }

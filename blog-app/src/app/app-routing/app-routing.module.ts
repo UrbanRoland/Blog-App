d19cf91c971b8a1ffd30b09707e88f35d57c572a
UrbanRoland <urolir@gmail.com>
2021-01-24 14:31:52 +0100

    feat: added basic routing

--- blog-app/src/app/app-routing/app-routing.module.ts
+++ blog-app/src/app/app-routing/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './../components/login/login.component';
+import { MainComponent } from './../components/main/main.component';
+import { HeaderComponent } from './../components/header/header.component';
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
 
-const routes: Routes = [];
+  { path: 'main', component: HeaderComponent },
+  {
+    path: '**',
+    redirectTo: '/main',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

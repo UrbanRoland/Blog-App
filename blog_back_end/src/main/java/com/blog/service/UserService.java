05cd55785564ff36c60e90d038f8555e9cf9b1fc
UrbanRoland <urolir@gmail.com>
2021-03-08 13:24:55 +0100

    feat added spring security and userdetails

--- blog_back_end/src/main/java/com/blog/service/UserService.java
+++ blog_back_end/src/main/java/com/blog/service/UserService.java
@@ -1,6 +1,9 @@
 package com.blog.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.blog.model.User;
@@ -8,18 +11,28 @@ import com.blog.repository.RoleRepository;
 import com.blog.repository.UserRepository;
 
 @Service
-public class UserService {
+public class UserService  implements UserDetailsService {
 
 	private UserRepository userRepository;
 	private RoleRepository roleRepository;
 	
 	
 	@Autowired
-	public UserService(UserRepository userRepository,RoleRepository roleRepository) {
+	public UserService(UserRepository userRepository, RoleRepository roleRepository) {
 		this.userRepository = userRepository;
 		this.roleRepository = roleRepository;
 	}
 	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByEmail(username);
+		if (user == null) {
+			throw new UsernameNotFoundException(username);
+		}
+
+		return new UserDetailsImpl(user);
+	}
+
 	public User saveUser(User user) {
 		return userRepository.save(user);
 	}

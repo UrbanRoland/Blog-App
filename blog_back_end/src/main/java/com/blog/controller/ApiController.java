e1e648c204426ce292c97ba39e33105b80738c96
UrbanRoland <urolir@gmail.com>
2021-03-11 15:07:41 +0100

    bugfix: removed unnesesary code

--- blog_back_end/src/main/java/com/blog/controller/ApiController.java
+++ blog_back_end/src/main/java/com/blog/controller/ApiController.java
@@ -1,18 +1,22 @@
 package com.blog.controller;
 
 
+import com.blog.model.User;
+import com.blog.service.UserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.blog.model.User;
-import com.blog.service.UserService;
-
 @RestController
-@RequestMapping(path = "api/",produces = "application/json")
+@RequestMapping(path = "test/",produces = "application/json")
 @CrossOrigin(origins = "http://localhost:4200")
 public class ApiController {
 
@@ -51,4 +55,16 @@ public class ApiController {
 		return userObj;
 	}
 	
+
+
+	
+	@GetMapping(path = "user/{id}")
+	public ResponseEntity<User> userById(@PathVariable Long id){
+
+	    if(userService.findById(id).get() == null){
+	        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+	    }
+	    return new ResponseEntity<>(userService.findById(id).get(), HttpStatus.OK);
+	}
+
 }

3e5b851b40200a7b9ae42513c7d1a14290c336c4
UrbanRoland <urolir@gmail.com>
2021-03-11 14:58:48 +0100

    feat: added new methods in repository packeges

--- blog_back_end/src/main/java/com/blog/repository/RoleRepository.java
+++ blog_back_end/src/main/java/com/blog/repository/RoleRepository.java
@@ -2,8 +2,12 @@ package com.blog.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 import com.blog.model.Role;
+import com.blog.model.RoleName;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
+    Optional<Role> findByName(RoleName roleName);
 }
--- blog_back_end/src/main/java/com/blog/repository/UserRepository.java
+++ blog_back_end/src/main/java/com/blog/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.blog.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 import com.blog.model.User;
 
 @Repository
@@ -10,4 +12,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
 	User findByEmail(String email);
 	User findByEmailAndPassword(String email,String password);
+	Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
 }

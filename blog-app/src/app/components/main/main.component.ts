1732877f2c9b02178ace6119378ca6664803aaf2
UrbanRoland <urolir@gmail.com>
2021-03-12 12:07:54 +0100

    feat: added new things to main - login,reg

--- blog-app/src/app/components/main/main.component.ts
+++ blog-app/src/app/components/main/main.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenStorageService } from 'src/app/auth/token-storage.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit {
+  info: any;
 
-  constructor() { }
+  constructor(private token: TokenStorageService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.info = {
+      token: this.token.getToken(),
+      username: this.token.getUsername(),
+      authorities: this.token.getAuthorities(),
+    };
   }
 
+  logout() {
+    this.token.signOut();
+    window.location.reload();
+  }
 }

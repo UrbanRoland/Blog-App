28e3fd018d534d0388189adfe3ff5c797de5c287
UrbanRoland <urolir@gmail.com>
2021-03-12 12:08:40 +0100

    feat: updated login page, new design, new inputs

--- blog-app/src/app/components/login/login.component.ts
+++ blog-app/src/app/components/login/login.component.ts
@@ -1,32 +1,65 @@
 import { Component, OnInit } from '@angular/core';
-import { RegistrationService } from '../../service/registration.service';
+import { RegistrationService } from '../../services/registration.service';
 import { User } from '../../classes/user';
 import { Route, Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthLoginInfo } from 'src/app/auth/login-info';
+import { TokenStorageService } from 'src/app/auth/token-storage.service';
+import { AuthService } from 'src/app/auth/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
+//implements OnInit
 export class LoginComponent implements OnInit {
-  constructor(private _service: RegistrationService, private _router: Router) {}
-
-  ngOnInit(): void {}
-
   hide = true;
-  msg = '';
-  user = new User();
+  form: any = {};
+  isLoggedIn = false;
+  isLoginFailed = false;
+  errorMessage = '';
+  roles: string[] = [];
+  private loginInfo: AuthLoginInfo;
 
-  loginUser() {
-    this._service.loginUserFromRemote(this.user).subscribe(
+  constructor(
+    private authService: AuthService,
+    private tokenStorage: TokenStorageService
+  ) {}
+
+  ngOnInit() {
+    if (this.tokenStorage.getToken()) {
+      this.isLoggedIn = true;
+      this.roles = this.tokenStorage.getAuthorities();
+    }
+  }
+
+  onSubmit() {
+    console.log(this.form);
+
+    this.loginInfo = new AuthLoginInfo(this.form.username, this.form.password);
+
+    this.authService.attemptAuth(this.loginInfo).subscribe(
       (data) => {
-        console.log('response receive');
-        this._router.navigate(['/']);
+        this.tokenStorage.saveToken(data.accessToken);
+        this.tokenStorage.saveUsername(data.username);
+        this.tokenStorage.saveAuthorities(data.authorities);
+
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+        this.roles = this.tokenStorage.getAuthorities();
+        this.reloadPage();
       },
       (error) => {
-        console.log('exception occured');
-        this.msg =
-          'Bad credentials, please enter your valid email and password!';
+        console.log(error);
+        this.errorMessage = error.error.message;
+        this.isLoginFailed = true;
       }
     );
+  }
+
+  reloadPage() {
+    window.location.reload();
+    window.location.href = 'http://localhost:4200/main';
   }
 }
